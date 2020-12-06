874184ac053e7424181a87127461fa45668a7257
Ruben <45242719+rubinho101@users.noreply.github.com>
2020-12-06 11:32:15 +0100

    Update MMM-01ZM.js

--- MMM-01ZM.js
+++ MMM-01ZM.js
@@ -1,42 +1,64 @@
-//var getJSON = require('get-json');
+var getJSON = require('get-json');
+
+ 
+function roomOne() {
+    getJSON('http://localhost:8000/?mac=58:2d:34:35:ad:00', 
+    function(error, response) {
+	    console.log(error);
+        console.log(response);
+        console.log("Room 1");
+    }
+);    
+}
+
+function roomTwo() {
+    getJSON('http://localhost:8000/?mac=58:2d:34:35:ad:00', 
+    function(error, response) {
+	    console.log(error);
+        console.log(response);
+        console.log("Room 2");
+    }
+);    
+}
+
+function roomThree() {
+    getJSON('http://localhost:8000/?mac=58:2d:34:35:ad:00', 
+    function(error, response) {
+	    console.log(error);
+        console.log(response);
+        console.log("Room 3");
+    }
+);    
+}
+
+function roomParse() {
+setTimeout(roomOne, 1);
+setTimeout(roomTwo, 10000);
+setTimeout(roomThree, 20000);
+}
+
+roomParse()
+
 
 Module.register("MMM-01ZM", {
-    defaults: {
-        foo: "°C"
-      },
+    defaults: {},
       start: function (){
         this.temperatur = ""
         var timer = setInterval(()=>{
           this.updateDom()
-        }, 10000)
+        }, 10000) 
+        setInterval(roomParse, 60000)
       },
       getDom: function() {
         var element = document.createElement("div")
         element.className = "myContent"
-        element.innerHTML = "Temperature " + "BLEtemp " + this.config.foo
+        element.innerHTML = "Temperature " + this.temperatur
         //var subElement = document.createElement("p")
         //subElement.id = "COUNT"
         //element.appendChild(subElement)
         return element
       },
       
-      notificationReceived: function(notification, payload, sender) {
-        switch(notification) {
-          case "DOM_OBJECTS_CREATED":
-            var timer = setInterval(()=>{
-              this.sendSocketNotification("DO_YOUR_JOB", this.count)
-              this.count++
-            }, 1000)
-            break
-        }
-      },
-      
-      socketNotificationReceived: function(notification, payload) {
-        switch(notification) {
-          case "I_DID":
-            var elem = document.getElementById("COUNT")
-            elem.innerHTML = "Count:" + payload
-            break
-        }
-      },
+  notificationReceived: function() {},
+  socketNotificationReceived: function() {},
   })
